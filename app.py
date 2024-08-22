8a942774833f98e2071cfb11466a2c2a2318028d
Eclipt <137305099+ecliipt@users.noreply.github.com>
2024-08-22 16:55:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import asyncio
+import sv
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
-
+@app.route('/create-user', methods=['POST'])
+def create_user():
+    user_data = request.get_json()
+    response = asyncio.run(sv.get_response(query=user_data['input']))
+    return jsonify(response), 201
 
 if __name__ == "__main__":
     app.run()
